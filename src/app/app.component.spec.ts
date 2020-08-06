c2cdc610d4b24d04d2d554bbbe2e3e661c768bef
bhaveshletter <bhaveshletter@gmail.com>
2020-08-06 17:41:44 +0530

    minor unit test fixes for bitcoin service

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,13 +21,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('chain-analysis');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain(
-      'chain-analysis app is running!'
-    );
-  });
 });
--- src/app/bitcoin.service.spec.ts
+++ src/app/bitcoin.service.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { BitcoinService } from './bitcoin.service';
 
 describe('BitcoinService', () => {
   let service: BitcoinService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(BitcoinService);
   });
 
