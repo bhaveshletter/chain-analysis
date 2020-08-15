676db569a0d4e55262f8a9369f345726818ec5f1
bhaveshletter <bhaveshletter@gmail.com>
2020-08-15 14:40:40 +0530

    heroku config

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-const express = require('express');
+const express = require("express"),
+  app = express();
 
-const app = express();
+app.use(express.static(__dirname + "/dist/chain-analysis"));
 
-app.use(express.static('./src'));
-
-app.get('/*', function (req, res) {
-  res.sendFile('index.html', { root: 'src' }
-  );
+app.get("/", function (req, res) {
+  res.sendFile("index.html", { root: "dist/chain-analysis" });
 });
 
 app.listen(process.env.PORT || 8080);
 
-console.log(`Running on port ${process.env.PORT || 8080}`)
+console.log(`Running on port ${process.env.PORT || 8080}`);
