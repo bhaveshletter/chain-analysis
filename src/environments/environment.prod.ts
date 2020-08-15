c835dbb88f6cbd8cbf54b046f83dd04f6eb083be
bhaveshletter <bhaveshletter@gmail.com>
2020-08-15 13:03:15 +0530

    css alignment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiURL: 'https://blockchain.proxy.pankaj.pyfox.dev',
+  wsURL: 'wss://ws.blockchain.info/inv',
+  bctAddressRegex: /^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$/,
 };
